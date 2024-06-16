7271208478b8cc62bd5be1b11a08f76f149d197a
kedirlluch <kedir980210@gmail.com>
2024-06-16 15:54:44 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,83 @@
+<script setup>
+
+</script>
+
+<template>
+  
+
+<nav class="bg-white border-gray-200 dark:bg-gray-900">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    
+    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <li>
+          <a href="#" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+<div class="grid grid-cols-2 md:grid-cols-4 gap-4 p-10">
+    <div class="grid gap-4">
+        <div>
+            <img class="h-auto max-w-full rounded-lg shadow-none transition-all duration-300 cursor-pointer hover:shadow-lg hover:shadow-gray-900 hover:scale-105" src="/media/kedir/524A56064A55E6EF/codes/tio/my-project/src/assets/WhatsApp Image 2024-02-25 at 20.52.51.jpeg" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg shadow-none transition-all duration-300 cursor-pointer hover:shadow-lg hover:shadow-gray-900 hover:scale-105" src="/media/kedir/524A56064A55E6EF/codes/tio/my-project/src/assets/WhatsApp Image 2024-02-25 at 21.00.23.jpeg" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg shadow-none transition-all duration-300 cursor-pointer hover:shadow-lg hover:shadow-gray-900 hover:scale-105" src="/media/kedir/524A56064A55E6EF/codes/tio/my-project/src/assets/WhatsApp Image 2024-02-25 at 21.03.57.jpeg" alt="">
+        </div>
+    </div>
+    <div class="grid gap-4">
+        <div>
+            <img class="h-auto max-w-full rounded-lg shadow-none transition-all duration-300 cursor-pointer hover:shadow-lg hover:shadow-gray-900 hover:scale-105" src="/media/kedir/524A56064A55E6EF/codes/tio/my-project/src/assets/WhatsApp Image 2024-02-25 at 21.06.15.jpeg" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg shadow-none transition-all duration-300 cursor-pointer hover:shadow-lg hover:shadow-gray-900 hover:scale-105" src="/media/kedir/524A56064A55E6EF/codes/tio/my-project/src/assets/WhatsApp Image 2024-02-25 at 21.07.24.jpeg" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg shadow-none transition-all duration-300 cursor-pointer hover:shadow-lg hover:shadow-gray-900 hover:scale-105" src="/media/kedir/524A56064A55E6EF/codes/tio/my-project/src/assets/WhatsApp Image 2024-02-25 at 21.09.59.jpeg" alt="">
+        </div>
+    </div>
+    <div class="grid gap-4">
+        <div>
+            <img class="h-auto max-w-full rounded-lg shadow-none transition-all duration-300 cursor-pointer hover:shadow-lg hover:shadow-gray-900 hover:scale-105" src="/media/kedir/524A56064A55E6EF/codes/tio/my-project/src/assets/WhatsApp Image 2024-02-25 at 21.11.39.jpeg" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg shadow-none transition-all duration-300 cursor-pointer hover:shadow-lg hover:shadow-gray-900 hover:scale-105" src="/media/kedir/524A56064A55E6EF/codes/tio/my-project/src/assets/WhatsApp Image 2024-02-25 at 21.12.51.jpeg" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg shadow-none transition-all duration-300 cursor-pointer hover:shadow-lg hover:shadow-gray-900 hover:scale-105" src="/media/kedir/524A56064A55E6EF/codes/tio/my-project/src/assets/WhatsApp Image 2024-02-25 at 21.14.37.jpeg" alt="">
+        </div>
+    </div>
+    <div class="grid gap-4">
+        <div>
+            <img class="h-auto max-w-full rounded-lg shadow-none transition-all duration-300 cursor-pointer hover:shadow-lg hover:shadow-gray-900 hover:scale-105" src="/media/kedir/524A56064A55E6EF/codes/tio/my-project/src/assets/WhatsApp Image 2024-02-25 at 21.15.52.jpeg" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg shadow-none transition-all duration-300 cursor-pointer hover:shadow-lg hover:shadow-gray-900 hover:scale-105" src="/media/kedir/524A56064A55E6EF/codes/tio/my-project/src/assets/WhatsApp Image 2024-02-25 at 21.18.53.jpeg" alt="">
+        </div>
+        <div>
+            <img class="h-auto max-w-full rounded-lg shadow-none transition-all duration-300 cursor-pointer hover:shadow-lg hover:shadow-gray-900 hover:scale-105" src="/media/kedir/524A56064A55E6EF/codes/tio/my-project/src/assets/WhatsApp Image 2024-02-25 at 21.20.37.jpeg" alt="">
+        </div>
+    </div>
+</div>
+
+</template>
+
